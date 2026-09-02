171055681a7fb46b6dcaf76b7f50710262f871b7
SteamOS User <marcelpetrusma84@gmail.coml>
2026-09-02 19:36:45 +0200

    supabase-js vastgezet op 2.113.0

--- supabase.js
+++ supabase.js
@@ -1,4 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2.113.0/+esm'
 
 export const supabase = createClient(
   'https://qmgatbphiplrfxrljtbe.supabase.co',
